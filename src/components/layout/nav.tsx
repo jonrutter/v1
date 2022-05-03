c148b05e0d85dd79bfd56a09349a0130104b4e7a
Jon Rutter <contact@jonrutter.io>
2022-05-03 19:11:00 -0400

    Fix nav styled link spacing
    Decrease navbar navlink spacing, since styled links now have
    horizontal padding (after adding focus ring).

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -49,7 +49,7 @@ export const NavBar: React.FC<NavBarProps> = ({
   toggleDrawer,
 }) => (
   <nav>
-    <ul className="hidden md:flex items-center space-x-8">
+    <ul className="hidden md:flex items-center space-x-6">
       {menu.map((link, index) => (
         <li key={index} className="font-heading font-medium text-lg">
           <NavLink to={link.url}>{link.name}</NavLink>
