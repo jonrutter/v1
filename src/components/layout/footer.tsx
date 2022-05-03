a251bb8ad8cb865b846918428642f80794800b62
Jon Rutter <contact@jonrutter.io>
2022-05-03 16:38:46 -0400

    Create Footer layout component

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
+// data
+import { menu, socialLinks } from '../../config';
+
+// components
+import { StyledLink } from '../styled-link';
+import { SocialLink } from '../social-link';
+import { Icon } from '../icon';
+
+type Props = {
+  short?: boolean;
+};
+
+/**
+ * Renders the site footer.
+ */
+export const Footer: React.FC<Props> = ({ short }) => (
+  <>
+    <div
+      className={`bg-slate-900 text-slate-50 px-6 md:px-12 pb-12 ${
+        short ? 'pt-28' : 'pt-72'
+      } text-lg`}
+    >
+      <footer className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col md:flex-row items-center text-center mb-16 md:justify-between">
+          <div className="mb-12 md:mr-8 md:mb-0">
+            <StaticImage
+              src="../../images/site-logo.svg"
+              alt="Jon Rutter"
+              className="max-w-[54px] h-auto"
+            />
+          </div>
+          <ul className="flex flex-col mb-8 md:flex-row md:items-center md:m-0 space-y-6 md:space-y-0 md:space-x-8">
+            {menu.map(({ url, name }, index) => (
+              <li key={index}>
+                <StyledLink
+                  to={url}
+                  className="hover:text-sky-400  focus:text-sky-400"
+                >
+                  {name}
+                </StyledLink>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex text-2xl space-x-4 items-center">
+            {socialLinks.map(({ url, name, icon }, index) => (
+              <li key={index}>
+                <SocialLink href={url} name={name}>
+                  <Icon name={icon} aria-hidden />
+                </SocialLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <p className="text-base text-center">
+          <StyledLink
+            as="a"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/rutterjt/v1/tree/main#license"
+            className="hover:text-sky-400  focus:text-sky-400 focus:ring-2 focus:ring-sky-400 hover:before:scale-x-0"
+          >
+            © {new Date().getFullYear()}, Jon Rutter
+          </StyledLink>
+        </p>
+      </footer>
+    </div>
+  </>
+);
