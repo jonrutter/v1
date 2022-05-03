8feed23d998d831f38b49b4ef7eddabec262f7fd
Jon Rutter <contact@jonrutter.io>
2022-05-03 16:42:20 -0400

    Create site main layout component
    Create Layout component to render:
    - SkipToContent link
    - Header
    - main content passed through as children
    - Footer

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// components
+import { Footer } from './footer';
+import { Header } from './header';
+import { SkipToContent } from './skip-to-main';
+
+type Props = {
+  withCTA?: boolean;
+};
+
+/**
+ * Main layout component for all pages on the site.
+ *
+ * @param props.withCTA - (Optional) renders a Call to action box at the bottom of the page.
+ */
+export const Layout: React.FC<Props> = ({ withCTA, children }) => (
+  <div className="overflow-x-hidden bg-white dark:bg-slate-900 transition-all text-slate-700 dark:text-slate-200">
+    <SkipToContent />
+    <Header />
+    <div id="main">
+      <main>{children}</main>
+      <Footer short={!withCTA} />
+    </div>
+  </div>
+);
