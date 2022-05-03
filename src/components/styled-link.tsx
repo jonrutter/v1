815e5617dff10f75f7cf69a4262a6331962e381d
Jon Rutter <contact@jonrutter.io>
2022-05-03 15:00:26 -0400

    Create polymorphic StyledLink component
    Create StyledLink. Default StyledLink to a GatsbyLink component.
    Base underline and focus ring styling on the text color, so
    you only need to specify the text's color for styling.

---
+++ src/components/styled-link.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Link } from 'gatsby';
+
+type Props<T extends React.ElementType> = {
+  as?: T;
+  className: string;
+};
+
+/**
+ * A styled polymorphic link component, that renders a styled GatsbyLink by default.
+ *
+ * @param props.as - The underlying component.
+ *
+ * All other props are passed directly to the underlying component.
+ */
+export const StyledLink = <T extends React.ElementType = typeof Link>({
+  as,
+  className,
+  children,
+  ...rest
+}: Props<T> & Omit<React.ComponentPropsWithoutRef<T>, keyof Props<T>>) => {
+  const Tag = as || Link;
+  return (
+    <Tag
+      className={clsx(
+        'leading-none overflow-hidden pb-1 relative transition-all before:block before:absolute before:bottom-0 before:left-underline before:h-[3px] before:w-6 before:rounded-md before:transition-all before:bg-current before:scale-x-0 hover:before:scale-x-100 outline-none focus:outline-none p-1 focus:ring-2 focus:ring-current',
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </Tag>
+  );
+};
