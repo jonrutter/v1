b4e1c4d75ce3ace5cc7067d1d902a2b6448459c6
Jon Rutter <contact@jonrutter.io>
2022-05-03 15:08:36 -0400

    Create SkipToMain component
    Create SkipToMain component, to allow keyboard users to bypass
    the header and nav links. Make SkipToMain link visually hidden
    when not focused.

---
+++ src/components/layout/skip-to-main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SkipToContent: React.FC = () => (
+  <a
+    href="#main"
+    className="fixed top-24 left-0 bg-white text-slate-900 dark:bg-slate-900 dark:text-slate-50 z-[9999] text-lg py-3 px-6 underline -translate-x-48 focus:translate-x-0 transition-all rounded-md shadow-md outline-none border-2 border-slate-900 dark:border-slate-50 ring-2 ring-sky-400"
+  >
+    Skip to content
+  </a>
+);
