ca9269b1e5cafc0c592885de16c5c5229423cc61
Jon Rutter <contact@jonrutter.io>
2022-05-10 19:07:16 -0400

    Update styling

--- src/components/layout/skip-to-main.tsx
+++ src/components/layout/skip-to-main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SkipToContent: React.FC = () => (
   <a
     href="#main"
-    className="fixed top-24 left-0 bg-white text-slate-900 dark:bg-slate-900 dark:text-slate-50 z-[9999] text-lg py-3 px-6 underline -translate-x-48 focus:translate-x-0 transition-all rounded-md shadow-md outline-none border-2 border-slate-900 dark:border-slate-50 ring-2 ring-sea-500"
+    className="fixed top-24 left-0 bg-white text-slate-900 dark:bg-slate-900 dark:text-slate-50 z-[9999] text-lg py-3 px-6 underline -translate-x-full focus:translate-x-1 transition-all rounded-md shadow-md outline-none focus:ring-2 focus:ring-slate-900 focus:dark:ring-slate-50"
   >
     Skip to content
   </a>
