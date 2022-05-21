4c2aaaaa11cf1cea5201992cb2a71cee524536f6
Jon Rutter <contact@jonrutter.io>
2022-05-21 13:37:00 -0400

    Update CTA box
    - Remove unused imports
    
    - Correct border radius re:figma design

--- src/components/sections/cta-box.tsx
+++ src/components/sections/cta-box.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 // components
 import { CTAButton } from '../buttons';
-import { H2, P } from '../typography';
 
 /**
  * Renders a page's CTA Box
  */
 export const CTABox: React.FC = () => (
   <div className="px-6 md:px-12 transition-all -mt-20 md:mt-0 bg-inherit text-slate-700 dark:text-slate-200">
-    <div className="max-w-site-md lg:max-w-site-lg mx-auto translate-y-1/2 bg-white dark:bg-gradient-to-br dark:from-slate-600 dark:to-slate-800 py-12 px-8 md:px-10 lg:py-16 lg:px-12 rounded-[2.5rem] shadow-lg transition-all overflow-hidden">
+    <div className="max-w-site-md lg:max-w-site-lg mx-auto translate-y-1/2 bg-white dark:bg-gradient-to-br dark:from-slate-600 dark:to-slate-800 py-12 px-8 md:px-10 lg:py-16 lg:px-12 rounded-3xl shadow-lg transition-all overflow-hidden">
       <section className="grid grid-cols-1 items-center justify-items-center max-w-sm mx-auto gap-4 md:gap-12 md:grid-cols-2 md:max-w-full lg:grid-cols-3 lg:gap-16">
         <h2 className="transition-all font-heading font-black text-3xl md:text-4xl text-slate-900 dark:text-slate-50">
           Let's get in touch
