fa293802c255ac2bc1f1319d4c22be20daee9c11
Jon Rutter <contact@jonrutter.io>
2022-05-25 17:09:13 -0400

    Update sent page content

--- src/pages/sent.tsx
+++ src/pages/sent.tsx
@@ -6,9 +6,9 @@ import { Layout, Seo, Hero, PrimaryButton } from '@/components';
 const SentPage: React.FC = () => (
   <Layout>
     <Seo title="Thank You | Jon Rutter" pathname="/sent" />
-    <Hero title="Thank you!">
+    <Hero title="Message sent" subtitle="Thank you!">
       <p className="mb-4">
-        Thank you for your message! I'll get back to you as soon as I can!
+        Thanks for your message! I'll get back to you as soon as I can!
       </p>
       <div className="text-center md:text-left mt-12 md:mt-8 mb-16">
         <PrimaryButton to="/" className="w-full sm:w-auto">
