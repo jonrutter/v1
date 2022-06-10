417893aaed00349c7dcb2458525f8367c6dc497a
Jon Rutter <contact@jonrutter.io>
2022-06-10 13:04:52 -0400

    Add custom test render
    - Create custom render function for tests
    
    - Create global import for test-utils

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/test/__mocks__/file-mock.js`,
     ...paths,
   },
+  moduleDirectories: ['node_modules', './test/modules'],
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script)/)`],
   globals: {
