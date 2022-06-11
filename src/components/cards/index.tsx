0c1ac61e1e4be774538e4ab5b82b8728c36dbcab
Jon Rutter <contact@jonrutter.io>
2022-06-11 14:54:13 -0400

    Create coming soon page

--- src/components/cards/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from './blog-card';
-export * from './portfolio-card';
-export * from './card';
--- src/components/form/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from './contact-form';
-export * from './error-message';
-export * from './inputs';
-export * from './label';
