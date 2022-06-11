83dcac7aeb2696f67bade866e503a041c5382e7c
Jon Rutter <contact@jonrutter.io>
2022-06-11 03:51:11 -0400

    Test TypeScript testing
    Add intentionally failing TS code to validate TS CI

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 import { ColorThemeContextProvider } from '@/styles/useColorTheme';
 
+import { BlogCard } from './cards';
+
 /**
  * Wraps all pages on the site.
  *
  * Currently includes the `<ColorThemeContextProvider />`
  */
 export const PageWrap: React.FC = ({ children }) => {
-  return <ColorThemeContextProvider>{children}</ColorThemeContextProvider>;
+  return (
+    <ColorThemeContextProvider>
+      {children}
+      <BlogCard />
+    </ColorThemeContextProvider>
+  );
 };
