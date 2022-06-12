285a8eed98fa3668e0619d2a7162a6b45054e693
Jon Rutter <contact@jonrutter.io>
2022-06-12 17:23:27 -0400

    Update site content

--- src/components/sections/cta-box.tsx
+++ src/components/sections/cta-box.tsx
@@ -10,15 +10,20 @@ export const CTABox: React.FC = () => (
   <div className="px-6 md:px-12 transition-all -mt-20 md:mt-0 bg-inherit text-slate-700 dark:text-slate-200">
     <div className="max-w-site-md lg:max-w-site-lg mx-auto translate-y-1/2 bg-white dark:bg-gradient-to-br dark:from-slate-600 dark:to-slate-800 py-12 px-8 md:px-10 lg:py-16 lg:px-12 rounded-3xl shadow-lg transition-all overflow-hidden">
       <section className="grid grid-cols-1 items-center justify-items-center max-w-sm mx-auto gap-4 md:gap-12 md:grid-cols-2 md:max-w-full lg:grid-cols-3 lg:gap-16">
-        <h2 className="transition-all font-heading font-black text-3xl md:text-4xl text-slate-900 dark:text-slate-50">
+        <h2
+          className="transition-all font-heading font-black text-3xl md:text-4xl text-slate-900 dark:text-slate-50"
+          data-testid="cta-box-header"
+        >
           Let's get in touch
         </h2>
         <p className="text-lg">
-          I'm currently available for work. Get in touch with me for inquiries,
-          or just to say hi!
+          Contact me to discuss anything web development related, or just to say
+          hi!
         </p>
         <div className="md:col-span-full lg:col-auto">
-          <PrimaryButton to="/contact">Contact me</PrimaryButton>
+          <PrimaryButton to="/contact" data-testid="cta-box-link">
+            Contact me
+          </PrimaryButton>
         </div>
       </section>
       <div className="shine opacity-50 dark:opacity-40" />
--- src/components/sections/home-hero.tsx
+++ src/components/sections/home-hero.tsx
@@ -14,10 +14,6 @@ export const HomeHero: React.FC = () => (
       to open source projects, and expanding my knowledge about web and software
       development.
     </p>
-    <p className="mb-4">
-      I'm currently available for work. Contact me for inquiries, or just to say
-      hello!
-    </p>
     <div className="text-center md:text-left mt-8 md:mt-8 flex flex-col sm:flex-row sm:items-center space-y-6 sm:space-y-0 sm:space-x-8">
       <PrimaryButton to="/contact" className="w-full sm:w-auto">
         Contact me
