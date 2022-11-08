6856df4764f3f7d4fc3cf8506c48121bcda7a125
Jon Rutter <contact@jonrutter.io>
2022-11-08 12:32:28 -0500

    Update layout component
    - Add layout to own module
    
    - Re-export from base index file
    
    - Apply style redesign

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// components
+import { Footer } from './footer';
+import { Nav } from './nav';
+import { SkipToContent } from './skip-to-main';
+
+type Props = {
+  blogPost?: boolean;
+};
+
+/**
+ * Main layout component for all pages on the site.
+ */
+export const Layout: React.FC<Props> = ({ blogPost, children }) => (
+  <div
+    className={`overflow-x-hidden max-w-[100vw] text-slate-600 dark:text-slate-200 ${
+      blogPost ? 'bg-slate-100 dark:bg-slate-900' : 'bg-white dark:bg-slate-800'
+    }`}
+  >
+    <SkipToContent />
+    <Nav />
+    <div id="main">
+      <main>{children}</main>
+      <Footer />
+    </div>
+  </div>
+);
