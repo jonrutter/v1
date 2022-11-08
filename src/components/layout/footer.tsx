ae88594705784f6f8b19794e04716d9db68c908d
Jon Rutter <contact@jonrutter.io>
2022-11-08 12:28:56 -0500

    Footer redesign
    - Apply style changes
    
    - Remove short prop
    
    - Add divider svg

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,21 +7,48 @@ import { menu, socialLinks } from '@/config';
 import { Icon, StyledLink, IconButton } from '@/components';
 import { Logo } from '../logo';
 
-type Props = {
-  short?: boolean;
-};
-
 /**
  * Renders the site footer.
  */
-export const Footer: React.FC<Props> = ({ short }) => (
-  <>
-    <div
-      className={`bg-slate-900 text-slate-50 px-6 md:px-12 pb-12 ${
-        short ? 'pt-28' : 'pt-72'
-      } text-lg`}
+export const Footer: React.FC = () => (
+  <footer className="relative w-full min-w-screen overflow-x-hidden pt-40 bg-white dark:bg-slate-800 text-slate-600 dark:text-slate-200">
+    {/* divider */}
+    <svg
+      height="148"
+      viewBox="0 0 1920 148"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute top-0 left-0 right-0 xl:w-full"
+      preserveAspectRatio="none"
     >
-      <footer className="max-w-screen-lg mx-auto">
+      <g clipPath="url(#clip0_1009_2359)">
+        <path
+          d="M1920 0H0V148H1920V0Z"
+          className="fill-slate-100 dark:fill-slate-900"
+        />
+        <path
+          d="M0 8L160 18L320 47L480 21L640 5L800 22L960 26L1120 0L1280 2L1440 31L1600 40L1760 13L1920 9V149H1760H1600H1440H1280H1120H960H800H640H480H320H160H0V8Z"
+          className="fill-slate-50 dark:fill-[#131E2F]"
+        />
+        <path
+          d="M0 38L160 60L320 41L480 83L640 82L800 50L960 36L1120 44L1280 61L1440 46L1600 37L1760 58L1920 61V149H1760H1600H1440H1280H1120H960H800H640H480H320H160H0V38Z"
+          className="fill-[#FBFCFD] dark:fill-[#182334]"
+        />
+        <path
+          d="M0 114L160 109L320 116L480 108L640 95L800 78L960 98L1120 105L1280 81L1440 100L1600 117L1760 108L1920 88V149H1760H1600H1440H1280H1120H960H800H640H480H320H160H0V114Z"
+          className="fill-white dark:fill-slate-800"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1009_2359">
+          <rect width="1920" height="148" className="fill-transparent" />
+        </clipPath>
+      </defs>
+    </svg>
+
+    {/* content */}
+    <div className={`px-6 md:px-12 pb-12 text-lg pt-4 md:pt-12 lg:pt-16`}>
+      <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col md:flex-row items-center text-center mb-16 md:justify-between">
           <div className="mb-12 md:mr-8 md:mb-0 w-12 h-auto">
             <Logo />
@@ -29,9 +56,7 @@ export const Footer: React.FC<Props> = ({ short }) => (
           <ul className="flex flex-col mb-8 md:flex-row md:items-center md:m-0 space-y-6 md:space-y-0 md:space-x-4 lg:space-x-8">
             {menu.map(({ url, name }, index) => (
               <li key={index}>
-                <StyledLink to={url} className="text-white">
-                  {name}
-                </StyledLink>
+                <StyledLink to={url}>{name}</StyledLink>
               </li>
             ))}
           </ul>
@@ -58,12 +83,12 @@ export const Footer: React.FC<Props> = ({ short }) => (
             target="_blank"
             rel="noreferrer"
             href="https://github.com/rutterjt/v1/tree/main#license"
-            className="text-white"
+            className="text-slate-500 dark:text-slate-300"
           >
             © {new Date().getFullYear()}, Jon Rutter
           </StyledLink>
         </p>
-      </footer>
+      </div>
     </div>
-  </>
+  </footer>
 );
